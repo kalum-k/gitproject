16abe96aa0d686531a22fd2e7ba36cde28bde897
kalum <go.line334g@gmail.com>
2020-09-03 12:00:22 +0700

    Update Use Claculator

--- CalculatorDriver.java
+++ CalculatorDriver.java
@@ -2,5 +2,6 @@ public class CalculatorDriver{
 	public static void main(String[] args){
 		Calculator c1 = new Calculator();
 		System.out.println(c1.add(3,6));
+		System.out.println(c1.muliply(4,2));
 	}
 }
