86143f87b4f4c8ce62e5b94213206a838a4253d7
kalum <go.line334g@gmail.com>
2020-09-03 11:11:27 +0700

    Implement Multiply

--- Calculator.java
+++ Calculator.java
@@ -1,10 +1,10 @@
 public class Calculator{
 	private int op1,op2;
 	public int muliply(int num1, int num2){
-		return 0;
+		return num1 * num2;
 	}
 	 public int add(int num1, int num2){ 
-                return num1+num2;
+                return num1 + num2;
         }
 	 public int subtract(int num1, int num2){ 
                 return 0;
