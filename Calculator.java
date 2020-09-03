a145cf5e3213aefff5cbbae9ad99a80390a285ec
“Sirinrat” <614259029@webmail.npru.ac.th>
2020-09-03 10:26:50 +0700

    create struture

---
+++ Calculator.java
@@ -0,0 +1,17 @@
+public class Calculator{
+	private int op1,op2;
+	public int muliply(int num1, int num2){
+		return 0;
+	}
+	 public int add(int num1, int num2){ 
+                return 0;
+        }
+	 public int subtract(int num1, int num2){ 
+                return 0;
+        }
+	 public int divide(int num1, int num2){ 
+                return 0;
+        }
+
+}	
+
